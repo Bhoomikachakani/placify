33f91e21be1dc26ed433f214f0cef89a2eef08f3
Chandana-D-K <chandanadk049@gmail.com>
2026-04-07 21:51:25 +0530

    Message

--- code.py
+++ code.py
@@ -1,9 +1,5 @@
 print(f"We are good friends")
 
 print("MAJOR PROJECT")
-<<<<<<< HEAD
-print("we are doing the major project in group of 4")
-=======
 
 print("Placify - platform")
->>>>>>> 7c8c78902cdc91b7c3480157d9d68de0b0b39c28
